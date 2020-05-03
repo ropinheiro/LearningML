bd34b51129ac0f43273c14af719d16251ba2c7f8
Rui Pinheiro <rui.pinheiro@together.pt>
2020-05-03 20:17:07 +0100

    HOML2 | Linear regression, singular value decomposition (with Skikit-Learn)

--- 01 - HOML2/04 - Training Models/01 - Linear Regression.py
+++ 01 - HOML2/04 - Training Models/01 - Linear Regression.py
@@ -10,8 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #
-# Sub-Section: Linear Regression
+# Linear Regression
+# (vanilla Python with Numpy)
 #
+print("--------------------------------------")
+print("Linear Regression without Scikit-Learn")
+print("--------------------------------------")
 X = 2 * np.random.rand(100, 1)
 y = 4 + 3 * X + np.random.randn(100,1)
 
@@ -19,7 +23,7 @@ plt.plot(X, y, 'b.')
 plt.axis([0,2,0,15])
 
 #
-# Sub-Section: The Normal Equation
+# Calculating the Normal Equation
 #
 X_b = np.c_[np.ones((100,1)),X]
 theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
@@ -34,8 +38,24 @@ plt.plot(X_new, y_predict, 'r-')
 plt.show()
 
 #
-# Sub-Section: ...
+# Linear Regression
+# (now with Scikit-Learn)
 #
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+print("-----------------------------------")
+print("Linear Regression with Scikit-Learn")
+print("-----------------------------------")
+print("Intercept=", lin_reg.intercept_)
+print("Coeficients=", lin_reg.coef_)
+print("Predict=", lin_reg.predict(X_new))
+
+# Note: The Scikit-Learn's LinearRegression uses a different formula to
+#       calculate the theta. Instead of Normal Equation, it uses what
+#       is called the Singular Value Decomposition (SVD).
+#       This is better than Normal Equation for a number of reasons.
+#       Learn more in HOML2, Chapter 4, pages 116-117.
 
 #
 # Sub-Section: ...
