1d9f7b850db4a4b0dea63324d81c4f50d3dc5270
Rui Pinheiro <rui.pinheiro@together.pt>
2020-05-06 00:08:24 +0100

    HOML2 | Early Stopping

--- 01 - HOML2/04 - Training Models/03 - SGD.py
+++ 01 - HOML2/04 - Training Models/03 - SGD.py
@@ -106,3 +106,42 @@ lasso_reg.fit(X, y.ravel())
 lasso_predict = lasso_reg.predict([[1.5]])
 
 print("Predict =", lasso_predict)
+
+print("--------------")
+print("Early Stopping")
+print("--------------")
+
+from sklearn.base import clone
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+
+# Prepare the data
+poly_scaler = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+    ("std_scaler", StandardScaler())    
+    ])
+X_train_poly_scaled = poly_scaler.fit_transform(X)
+X_val_poly_scaled = poly_scaler.transform(y)
+
+sgd_reg = SGDRegressor(max_iter=1, tol=-np.infty, warm_start=True,
+                       penalty=None, learning_rate="constant", eta0=0.0005)
+
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y.ravel())
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y, y_val_predict)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
+
+print("Best epoch = ", best_epoch)
+print("Validation error = ", minimum_val_error)
+
+# Note: with warm_start = True, when the fit() method is called, it continues
+#       training where it left off, instead of restarting from scratch.
