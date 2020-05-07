5d89ee76315354190644d43f6692bd8476bc230b
Rui Pinheiro <rui.pinheiro@together.pt>
2020-05-07 20:36:06 +0100

    HOML2 | Nonlinear SVM Classification

---
+++ 01 - HOML2/05 - Support Vector Machines/05-02 - Nonlinear SVM Classification.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+HOML2 - Chapter 5 - Support Vector Machines
+        Section: Nonlinear SVM Classification
+
+@author: rmp / rpinheiro / ropinheiro
+"""
+
+import numpy as np
+
+print("-------------")
+print("Moons Dataset")
+print("-------------")
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import LinearSVC
+
+X, y = make_moons(n_samples=100, noise=0.15)
+
+def plot_dataset(X, y, axes):
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+    plt.axis(axes)
+    plt.grid(True, which='both')
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
+
+polynomial_svm_clf = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=3)),
+    ("scaler", StandardScaler()),
+    ("svm_clf", LinearSVC(C=10, loss="hinge"))
+    ])
+
+polynomial_svm_clf.fit(X, y)
+
+print("Predict = ", polynomial_svm_clf.predict([[5.5, 1.7]]))
+
+def plot_predictions(clf, axes):
+    x0s = np.linspace(axes[0], axes[1], 100)
+    x1s = np.linspace(axes[2], axes[3], 100)
+    x0, x1 = np.meshgrid(x0s, x1s)
+    X = np.c_[x0.ravel(), x1.ravel()]
+    y_pred = clf.predict(X).reshape(x0.shape)
+    y_decision = clf.decision_function(X).reshape(x0.shape)
+    plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
