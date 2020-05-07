04dfb1b070cda48882c5304f827809810817062c
Rui Pinheiro <rui.pinheiro@together.pt>
2020-05-07 20:42:42 +0100

    HOML2 | Polynomial Kernel

--- 01 - HOML2/05 - Support Vector Machines/05-02 - Nonlinear SVM Classification.py
+++ 01 - HOML2/05 - Support Vector Machines/05-02 - Nonlinear SVM Classification.py
@@ -55,3 +55,42 @@ def plot_predictions(clf, axes):
 plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
 plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
 plt.show()
+
+
+print("-----------------")
+print("Polynomial Kernel")
+print("-----------------")
+
+from sklearn.svm import SVC
+
+# Polynomial Kernel with degree 3 and coeficient 1
+poly_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+    ])
+poly_kernel_svm_clf.fit(X, y)
+
+# Polynomial Kernel with degree 10 and coeficient 100
+poly100_kernel_svm_clf = Pipeline([
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="poly", degree=10, coef0=100, C=5))
+    ])
+poly100_kernel_svm_clf.fit(X, y)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10.5, 4), sharey=True)
+
+plt.sca(axes[0])
+plot_predictions(poly_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=3, r=1, C=5$", fontsize=18)
+
+plt.sca(axes[1])
+plot_predictions(poly100_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+plt.ylabel("")
+plt.show()
+
+# Note: if the model starts overfitting, just decrease the degree.
+#       if it is underfitting, then increase the degree.
+
